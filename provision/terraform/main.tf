10777f3551cceee710401544447cfc71217dc58a
Clyde Tedrick <iamclyde@gmail.com>
2020-01-28 19:00:18 -0500

    initial commit

---
+++ provision/terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-pour-moi"
+    key    = "clyde/fightthetide"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "fighttheti.de"
+}
+
+module "website" {
+  source         = "git::https://github.com/cloudposse/terraform-aws-s3-website.git?ref=0.8.0"
+  namespace      = "clyde"
+  stage          = "dev"
+  name           = "app"
+  hostname       = "www.fighttheti.de"
+  parent_zone_id = aws_route53_zone.primary.zone_id
+}
+
+resource "null_resource" "remove_and_upload_to_s3" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ${path.module}/../../html s3://${module.website.s3_bucket_name}"
+  }
+}
+
+output "meep" {
+  value = module.website
+}
